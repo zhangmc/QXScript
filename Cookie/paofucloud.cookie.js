c4c8d2c75550aac3c880c69e04504200ed26f51a
ZMC <zmc0927@gmail.com>
2020-02-15 14:27:21 +0800

    update

---
+++ Cookie/paofucloud.cookie.js
@@ -0,0 +1,23 @@
+var paofucloud = $request.headers["Cookie"];
+console.log("aaaa"+paofucloud)
+if (paofucloud) {
+  if ($prefs.valueForKey("CookiePFC") != undefined) {
+    if ($prefs.valueForKey("CookiePFC") != paofucloud) {
+      var cookie = $prefs.setValueForKey(paofucloud, "CookiePFC");
+      if (!cookie) {
+        $notify("更新泡芙云签到Cookie失败‼️", "", "")
+      } else {
+        $notify("更新泡芙云签到Cookie成功 🎉", "", "")
+      }
+    }
+  } else {
+    var cookie = $prefs.setValueForKey(paofucloud, "CookiePFC");
+    if (!cookie) {
+      $notify("首次写入泡芙云Cookie失败‼️", "", "")
+    } else {
+      $notify("首次写入泡芙云Cookie成功 🎉", "", "")
+    }
+  }
+}
+console.log("bbb"+paofucloud)
+$done({})
