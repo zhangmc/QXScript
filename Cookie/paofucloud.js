d764a87bc1ec4ffeac4daea6afc0a4e9b28bb1e3
ZMC <zmc0927@gmail.com>
2020-02-16 10:37:36 +0800

    update

--- Cookie/paofucloud.js
+++ Cookie/paofucloud.js
@@ -4,23 +4,24 @@ var bonus = {
     "Cookie": $prefs.valueForKey("CookiePFC"),
   }
 };
-bonus.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36'
-bonus.headers['Host'] = 'paofu.cloud'
-bonus.headers['Origin'] = 'https://paofu.cloud'
-bonus.headers['Sec-Fetch-Site'] = 'same-origin'
-bonus.headers['Sec-Fetch-Mode'] = 'cors'
-bonus.headers['Referer'] = 'https://paofu.cloud/user'
+bonus.headers['User-Agent'] = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.130 Safari/537.36';
+bonus.headers['Host'] = 'paofu.cloud';
+bonus.headers['Origin'] = 'https://paofu.cloud';
+bonus.headers['Sec-Fetch-Site'] = 'same-origin';
+bonus.headers['Sec-Fetch-Mode'] = 'cors';
+bonus.headers['Referer'] = 'https://paofu.cloud/user';
 $task.fetch(bonus).then(response => {
-  let data = response.body
-  let result = JSON.parse(data)
-  let title = "PFC"
-  if (result && result.ret == 1) {
-    let subTitle = `签到结果: 成功`
-    let detail = `${result.msg}, 剩余流量 ${result.traffic}。`
-    $notify(title, subTitle, detail)
+  let data = response.body;
+  let result = JSON.parse(data);
+  console.log(result);
+  let title = "PFC";
+  if (result && result.ret == "1") {
+    let subTitle = `签到结果: 成功`;
+    let detail = `${result.msg}, 剩余流量 ${result.traffic}。`;
+    $notify(title, subTitle, detail);
   }
   else {
-    let subTitle = `签到结果: 失败`
-    $notify(title, subTitle, "")
+    let subTitle = `签到结果: 失败`;
+    $notify(title, subTitle, "");
   }
 });
