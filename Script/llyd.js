e4f21d0ca5432c9e67e03ad29bc211dce5b8a31e
ZMC <zmc0927@gmail.com>
2020-03-08 13:53:23 +0800

    update

--- Script/llyd.js
+++ Script/llyd.js
@@ -9,18 +9,26 @@ MitM = vira.llsapp.com
 
 */
 
-if ($response.statusCode == 200 && $request.method == 'GET') {
-	const bodyObj = JSON.parse($response.body);
+var body = $response.body;
+var url = $request.url;
+var obj = JSON.parse(body);
 
-	if ($request.url.indexOf('limitation') != -1) {
-		bodyObj.modules = [];
-		bodyObj.auditionDuration = 72000;
-	} else {
-		bodyObj.from = 1482071586
-		bodyObj.to = 1671373986
-	}
+const vip = '/api/v2/readings/limitation';
+const time = '/api/v2/readings/accessible';
 
-	$done({body: JSON.stringify(bodyObj)})
-} else {
-	$done({})
+if (url.indexOf(vip) != -1) {
+	obj["modules"] = [];
+	obj["auditionDuration"] = 7200;
+	body = JSON.stringify(obj);
 }
+
+if (url.indexOf(time) != -1) {
+	obj["from"] = 1482071586;
+	obj["to"] = 1671373986;
+	body = JSON.stringify(obj);
+}
+
+$done({body});
+/**
+ * @supported 9E1BBA07A0EF
+ */
