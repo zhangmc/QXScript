0954a7a620ba68e6a67505d53d784326f0d58c40
ZMC <zmc0927@gmail.com>
2021-08-22 18:14:09 +0800

    update

--- dog_scripts/jd_jdzz.js
+++ dog_scripts/jd_jdzz.js
@@ -140,6 +140,7 @@ function getUserInfo() {
           console.log(`${JSON.stringify(err)}`)
           console.log(`${$.name} API请求失败，请检查网路重试`)
         } else {
+          console.log(`\n【京东账号好友互助码】${data.data.shareTaskRes.itemId}\n`);
           if (safeGet(data)) {
             data = JSON.parse(data);
             console.log(`\n【京东账号${$.index}（${$.nickName || $.UserName}）的${$.name}好友互助码】${data.data.shareTaskRes.itemId}\n`);
